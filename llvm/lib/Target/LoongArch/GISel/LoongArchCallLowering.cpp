6f9128b411804bd13ca0e3222c0a270bdc5c1fb4
Qi Zhao <zhaoqi01@loongson.cn>
2024-11-14 21:09:59 +0800

    [LoongArch][GlobalISel] Impl lowerFormalArguments for calling conv
    Add IncomingValueHandler and IncomingValueAssigner, and
    implement minimal support for lowering formal arguments.
    Simple non-aggregate integer, pointer types and  byval/sret
    pointer arguments are supported.
    
    TODO: Support the number of arguments exceeds the number
    of registers.

--- llvm/lib/Target/LoongArch/GISel/LoongArchCallLowering.cpp
+++ llvm/lib/Target/LoongArch/GISel/LoongArchCallLowering.cpp
@@ -20,6 +20,9 @@
 
 using namespace llvm;
 
+LoongArchCallLowering::LoongArchCallLowering(const LoongArchTargetLowering &TLI)
+    : CallLowering(&TLI) {}
+
 namespace {
 
 struct LoongArchOutgoingValueAssigner
@@ -85,9 +88,6 @@ private:
 
 } // namespace
 
-LoongArchCallLowering::LoongArchCallLowering(const LoongArchTargetLowering &TLI)
-    : CallLowering(&TLI) {}
-
 bool LoongArchCallLowering::lowerReturn(MachineIRBuilder &MIRBuilder,
                                         const Value *Val,
                                         ArrayRef<Register> VRegs,
@@ -123,13 +123,110 @@ bool LoongArchCallLowering::lowerReturn(MachineIRBuilder &MIRBuilder,
   return true;
 }
 
+namespace {
+
+struct LoongArchIncomingValueAssigner
+    : public CallLowering::IncomingValueAssigner {
+private:
+  // The function used internally to assign args - we ignore the AssignFn stored
+  // by IncomingValueAssigner since LoongArch implements its CC using a custom
+  // function with a different signature.
+  LoongArchTargetLowering::LoongArchCCAssignFn *LoongArchAssignFn;
+
+  // Whether this is assigning args from a return.
+  bool IsRet;
+
+public:
+  LoongArchIncomingValueAssigner(
+      LoongArchTargetLowering::LoongArchCCAssignFn *LoongArchAssignFn_,
+      bool IsRet)
+      : CallLowering::IncomingValueAssigner(nullptr),
+        LoongArchAssignFn(LoongArchAssignFn_), IsRet(IsRet) {}
+
+  bool assignArg(unsigned ValNo, EVT OrigVT, MVT ValVT, MVT LocVT,
+                 CCValAssign::LocInfo LocInfo,
+                 const CallLowering::ArgInfo &Info, ISD::ArgFlagsTy Flags,
+                 CCState &State) override {
+    MachineFunction &MF = State.getMachineFunction();
+    const DataLayout &DL = MF.getDataLayout();
+    const LoongArchSubtarget &Subtarget = MF.getSubtarget<LoongArchSubtarget>();
+
+    return LoongArchAssignFn(DL, Subtarget.getTargetABI(), ValNo, ValVT,
+                             LocInfo, Flags, State, /*IsFixed=*/true, IsRet,
+                             Info.Ty);
+  }
+};
+
+struct LoongArchIncomingValueHandler
+    : public CallLowering::IncomingValueHandler {
+  LoongArchIncomingValueHandler(MachineIRBuilder &B, MachineRegisterInfo &MRI)
+      : IncomingValueHandler(B, MRI) {}
+
+  Register getStackAddress(uint64_t MemSize, int64_t Offset,
+                           MachinePointerInfo &MPO,
+                           ISD::ArgFlagsTy Flags) override {
+    llvm_unreachable("not implemented");
+  }
+
+  void assignValueToAddress(Register ValVReg, Register Addr, LLT MemTy,
+                            const MachinePointerInfo &MPO,
+                            const CCValAssign &VA) override {
+    llvm_unreachable("not implemented");
+  }
+
+  void assignValueToReg(Register ValVReg, Register PhysReg,
+                        const CCValAssign &VA) override {
+    // Copy argument received in physical register to desired VReg.
+    MIRBuilder.getMBB().addLiveIn(PhysReg);
+    MIRBuilder.buildCopy(ValVReg, PhysReg);
+  }
+};
+
+} // namespace
+
 bool LoongArchCallLowering::lowerFormalArguments(
     MachineIRBuilder &MIRBuilder, const Function &F,
     ArrayRef<ArrayRef<Register>> VRegs, FunctionLoweringInfo &FLI) const {
+  // Early exit if there are no arguments.
   if (F.arg_empty())
     return true;
 
-  return false;
+  // TODO: Support vararg functions.
+  if (F.isVarArg())
+    return false;
+
+  // TODO: Support all argument types.
+  for (auto &Arg : F.args()) {
+    if (Arg.getType()->isIntOrPtrTy())
+      continue;
+    return false;
+  }
+
+  MachineFunction &MF = MIRBuilder.getMF();
+  const DataLayout &DL = MF.getDataLayout();
+  CallingConv::ID CC = F.getCallingConv();
+
+  SmallVector<ArgInfo, 32> SplitArgInfos;
+  unsigned Index = 0;
+  for (auto &Arg : F.args()) {
+    // Construct the ArgInfo object from destination register and argument type.
+    ArgInfo AInfo(VRegs[Index], Arg.getType(), Index);
+    setArgFlags(AInfo, Index + AttributeList::FirstArgIndex, DL, F);
+
+    // Handle any required merging from split value types from physical
+    // registers into the desired VReg. ArgInfo objects are constructed
+    // correspondingly and appended to SplitArgInfos.
+    splitToValueTypes(AInfo, SplitArgInfos, DL, CC);
+
+    ++Index;
+  }
+
+  LoongArchIncomingValueAssigner Assigner(LoongArch::CC_LoongArch,
+                                          /*IsRet=*/false);
+  LoongArchIncomingValueHandler Handler(MIRBuilder, MF.getRegInfo());
+
+  return determineAndHandleAssignments(Handler, Assigner, SplitArgInfos,
+                                       MIRBuilder, CC, F.isVarArg());
 }
 
 bool LoongArchCallLowering::lowerCall(MachineIRBuilder &MIRBuilder,
